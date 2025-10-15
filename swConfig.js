01925f59abde98257c4087c578271fe9e48c11c7
ChetHefton <chetthefton@gmail.com>
2025-10-15 15:53:09 -0500

    bug fix attempt 2
    scope consistency

--- swConfig.js
+++ swConfig.js
@@ -3,7 +3,7 @@
     //waits until page finishes loading
     window.addEventListener('load', () => {
       //tell the browser to activate service-worker.js
-      navigator.serviceWorker.register('/service-worker.js')
+      navigator.serviceWorker.register('./service-worker.js', { scope: './' })
         .then(reg => console.log('service-worker registered', reg))
         .catch(console.error);
     });
