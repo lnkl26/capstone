5ee0c9e17c50dfa6db7887122d3b88e20f381fec
CjBallesil <cjballesil@att.net>
2025-12-04 10:34:14 -0600

    bump service worker version

--- service-worker.js
+++ service-worker.js
@@ -1,4 +1,4 @@
-const VERSION = 'v19';  //bump this when we make big changes, if u have a weird bug, bump this and refresh
+const VERSION = 'v20';  //bump this when we make big changes, if u have a weird bug, bump this and refresh
 const CACHE = `static-${VERSION}`;  //unique cache name per version
 const ASSETS = [  //files to be available offline immediately
   './',
