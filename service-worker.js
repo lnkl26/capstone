a75f5e017e8004b2df5cd76e83464851df6a3432
Leann Kahal <kahalleann05@gmail.com>
2025-12-06 18:05:34 -0600

    bumping service worker

--- service-worker.js
+++ service-worker.js
@@ -1,4 +1,4 @@
-const VERSION = 'v23';  //bump this when we make big changes, if u have a weird bug, bump this and refresh
+const VERSION = 'v23.0.1';  //bump this when we make big changes, if u have a weird bug, bump this and refresh
 const CACHE = `static-${VERSION}`;  //unique cache name per version
 const ASSETS = [  //files to be available offline immediately
   './',
