696852d9e85d43031c9205a09e7b9ac4d86d8432
Leann Kahal <kahalleann05@gmail.com>
2026-04-01 20:10:33 -0500

    update service worker to v31.9.0

--- service-worker.js
+++ service-worker.js
@@ -1,4 +1,4 @@
-const VERSION = 'v31.8.0';  //bump this when we make big changes, if u have a weird bug, bump this and refresh
+const VERSION = 'v31.9.0';  //bump this when we make big changes, if u have a weird bug, bump this and refresh
 const CACHE = `static-${VERSION}`;  //unique cache name per version
 const ASSETS = [  //files to be available offline immediately
 './',
